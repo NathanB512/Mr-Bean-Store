6e7eaf14e87d59fae9fdaf29d8011220c9c5bb86
NathanB512 <nathan.brown909@outlook.com>
2023-05-26 06:13:35 +0100

    Boiler plate for gender categories (bug addressed)

--- lookbook/categories/urls.py
+++ lookbook/categories/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path("men/brand", views.menBrand_view, name="menBrand"),
-    path("men/brand", views.menCategory_view, name="menCategory"),
-    path("men/brand", views.womenBrand_view, name="womenBrand"),
-    path("men/brand", views.womenCategory_view, name="womenCategory"),
+    path("men/category", views.menCategory_view, name="menCategory"),
+    path("women/brand", views.womenBrand_view, name="womenBrand"),
+    path("women/category", views.womenCategory_view, name="womenCategory"),
 ]
